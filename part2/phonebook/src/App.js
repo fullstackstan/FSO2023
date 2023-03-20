f4160e53eb99d7e787f5e55884c93884422c69fa
Stanley Brown <fullstackstan@gmail.com>
2023-03-20 11:35:07 -0500

    exercise2-8 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,49 +1,62 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: "Arto Hellas", number: "2222222-222" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  const handleNameChange = (event)=>{
-    setNewName(event.target.value)
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
     console.log(event.target.value);
-  }
+  };
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+    console.log(event.target.value);
+  };
 
-  const handleSubmit = (event)=>{
-    event.preventDefault()
-    for (let i=0;i<persons.length;i++){
-      if (persons[i].name===newName){
-        alert(`${newName} is already in the phone book`)
-        setNewName('')
-        return
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newName) {
+        alert(`${newName} is already in the phone book`);
+        setNewName("");
+        setNewNumber("");
+        return;
       }
     }
     const personObject = {
       name: newName,
-    }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
+      number: newNumber,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input value={newName} onChange={handleNameChange}/>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person=>(
-        <li>{person.name}</li>
+      {persons.map((person) => (
+        <li key={person.name}>
+          {person.name} {person.number}
+        </li>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
