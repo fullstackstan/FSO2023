db43de11f03f233ca86e83305152706fff337a15
Stanley Brown <fullstackstan@gmail.com>
2023-03-20 23:23:28 -0500

    exercise2-9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "2222222-222" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+  const [filterName, setFilterName] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
@@ -13,6 +17,10 @@ const App = () => {
   };
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+    console.log(event.target.value);
+  };
+  const handleFilterChange = (event) => {
+    setFilterName(event.target.value);
     console.log(event.target.value);
   };
 
@@ -29,15 +37,25 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
     setPersons(persons.concat(personObject));
     setNewName("");
     setNewNumber("");
   };
 
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterName.toLowerCase())
+  );
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input value={filterName} onChange={handleFilterChange} />
+      </div>
+      <h4>add a new entry</h4>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -50,8 +68,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <li key={person.name}>
+      {filteredPersons.map((person) => (
+        <li key={person.id}>
           {person.name} {person.number}
         </li>
       ))}
